5e7bf89610a4787b1b181af21b2dde8dae91b637
markfoleyie <mark.foley@tudublin.ie>
2021-03-04 16:18:13 +0000

    Added/updated some utilities and modified .gitignore

--- utilities/reproject_point.py
+++ utilities/reproject_point.py
@@ -1,5 +1,6 @@
 import pyproj
 from fiona.crs import from_epsg, from_string, to_string
+from shapely.geometry import Point
 
 
 def reproject(point, source_epsg_code, target_epsg_code):
@@ -8,3 +9,14 @@ def reproject(point, source_epsg_code, target_epsg_code):
 
     return (target_x, target_y)
 
+
+def main():
+    point_4326 = Point(-6.33, 53.33)
+    point_29902 = Point(reproject(point_4326, 4326, 29902))
+    point_2157 = Point(reproject(point_4326, 4326, 2157))
+
+    print(f"Source point is {point_4326}\n29902: {point_29902}\n2157: {point_2157}")
+
+
+if __name__ == "__main__":
+    main()
